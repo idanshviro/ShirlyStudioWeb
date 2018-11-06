3521cf051b77ed593082dd58069c5171be2ddb37
Idan Shviro <idanshviro@gmail.com>
2018-11-06 21:43:51 +0200

    Added facebook to the homepage + edit the navbar

---
+++ ShirlyStudio/Models/cluster-customizer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShirlyStudio.Models
+{
+    public class cluster_customizer
+    {
+        [Key]
+        [Required]
+        public int ClusterResulterID { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public int ClusterRes { get; set; }
+    }
+}
